582930b951f55d3ff01477c8807c80ffb61c257d
Cooper Myton <cooper.myton@gmail.com>
2026-02-15 21:46:27 -0800

    Refreshed statistics, got new dataset, starting checking, sorting and cleaning

--- data_test.py
+++ data_test.py
@@ -2,6 +2,23 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('TeamStatistics.csv')
+#List of attributes which are unnecessary
+COLS_TO_DELETE = ["gameId", "teamID", "opponentTeamId", "coachId", "timeoutsRemaining", "timesTied"]
 
-df = df.drop("gameId", axis=1, inplace=True)
+games = pd.read_csv('TeamStatistics.csv')
+advanced = pd.read_csv('TeamStatisticsAdvanced.csv')
+
+#Converting Dates and sorting
+games["gameDateTimeEst"] = pd.to_datetime(games["gameDateTimeEst"])
+advanced["gameDateTimeEst"] = pd.to_datetime(advanced['gameDateTimeEst'], errors="coerce")
+
+games = games.sort_values(["teamId", "gameDateTimeEst"])
+advanced = advanced.sort_values(["teamId", "gameDateTimeEst"])
+
+
+#Sanity checks
+print(games[["teamId", "gameDateTimeEst"]].head(10))
+#print(games.head())
+#print(advanced.head())
+#print(games.info())
+#print(advanced.info())
